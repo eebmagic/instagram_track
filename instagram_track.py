bc5666a13c386888c0cb3d041f9bd43777288e27
Ethan B <eebmagic@gmail.com>
2020-07-29 01:35:29 -0500

    updated and fixed date issue

--- instagram_track.py
+++ instagram_track.py
@@ -2,31 +2,51 @@ import requests
 import time
 import json
 
-recordFilePATH = "/home/ethanbolton/pythonTools/instagram_track/pastData.json"
+def parse_line(l):
+    start = l.index('content="')+len('content="')
+    end = l.index(' - ')
+    l = l[start:end]
 
-current_time = time.strftime('%x - %X')
+    out = {}
+    for x in l.split(', '):
+        x = x.split(' ')
+        out[x[1]] = x[0]
+
+    return out
+
+
+# Constants
+recordFilePATH = "/home/ethanbolton/pythonTools/instagram_check/pastData.json"
+urlFile = "/home/ethanbolton/pythonTools/instagram_check/URL.txt"
+key = '<meta property="og:description" content="'
+
+
+# Make current time string
+current_time = time.strftime('%Y/%m/%d - %X')
 print(current_time)
 
-url = "https://www.instagram.com/ee.bolton/"
+# Load url
+with open(urlFile) as file:
+    url = file.read().strip()
+
 r = requests.get(url)
 r_html = r.text
+lines = r_html.split('\n')
 
-labels = r_html[r_html.index('<meta content="')+len('<meta content="'):]
-labels = labels[:labels.index(' - ')]
-labels = labels.split(', ')
+# Find and parse data
+for line in lines:
+    if key in line:
+        print(line)
+        values = parse_line(line)
+        print(current_time, values)
 
-newData = {}
-for label in labels:
-	sections = label.split(' ')
-	newData[sections[1]] = sections[0]
-
-print(labels)
-print(newData)
-
+# Open old data
 with open(recordFilePATH, "r") as file:
-	oldData = json.load(file)
+	data = json.load(file)
 
-oldData[current_time] = newData
+data[current_time] = values
+print(data)
 
+# Save new data
 with open(recordFilePATH, "w") as write_file:
-	json.dump(oldData, write_file)
+	json.dump(data, write_file)
